5337ca2dbe74b12a48e3b1a7251e1742366ef0d2
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-03 02:17:45 +0600

    MongoBD database with .env file setup done

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 // midleware
 app.use(cors());
 app.use(express.json());
+
+
+// from Mongobd  database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('genious car server is running');
